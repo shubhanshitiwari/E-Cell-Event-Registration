184d9a3ebc2ea673366091d4475c62eaf1fd169e
Pavitra Pandey <154352488+2005pavitra@users.noreply.github.com>
2024-08-23 19:09:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,141 @@ cont.forEach(con => {
     }
     
 });
+
+
+let cont = document.querySelectorAll(".container")
+cont.forEach(con => {
+    con.onmousemove = function (e) {
+        let x = e.pageX - con.offsetLeft;
+        let y = e.pageY - con.offsetTop;
+
+        con.style.setProperty("--x", x + "px")
+        con.style.setProperty("--y", y + "px")
+    }
+
+});
+
+
+
+
+// const fname = document.getElementById("fname")
+// const lname = document.getElementById("lname")
+// const email = document.getElementById("email")
+// const college = document.getElementById("college")
+// const branch = document.getElementById("branch")
+// const eventUpdate = document.querySelectorAll(".event-update")
+
+// const nameError = document.getElementById("nameError")
+// const emailError = document.getElementById("emailError")
+// const branchError = document.getElementById("branchError")
+// const collegeError = document.getElementById("collegeError")
+// const agreeError = document.getElementById("agreeError");
+
+// nameError.innerHTML = "";
+// emailError.innerHTML = "";
+// agreeError.innerHTML = "";
+// collegeError.innerHTML = "";
+// branchError.innerHTML = "";
+
+// let isValid = true;
+
+// if (fname === "") {
+//     nameError.innerHTML = "Please Enter your First name.";
+//     isValid = false;
+// }
+
+// if (email === "" || !email.includes("@")) {
+//     emailError.innerHTML = "Please enter your email.";
+//     isValid = false;
+// }
+
+// if (college === "") {
+//     collegeError.innerHTML = "Please enter your college.";
+//     isValid = false;
+// }
+
+// if (branch === "") {
+//     branchError.innerHTML = "Please select your branch.";
+//     isValid = false;
+// }
+
+// if (!eventUpdate) {
+//     agreeError.innerHTML = "Please agree to above information.";
+//     isValid = false;
+// }
+
+
+
+const college = document.getElementById("college")
+const branch = document.getElementById("branch")
+
+function clearErrors() {
+
+    errors = document.getElementsByClassName('formerror');
+    for (let item of errors) {
+        item.innerHTML = "";
+    }
+
+
+}
+function seterror(id, error) {
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm() {
+    var returnval = true;
+    clearErrors();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    var name = document.forms['myForm']["fname"].value;
+    if (name.length < 5) {
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (name.length === 0) {
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    var email = document.forms['myForm']["femail"].value;
+    if (email.length > 30) {
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+    if (!email.includes("@")) {
+        seterror("email", "*Email should contain @ symbol");
+    }
+
+    if (college === "") {
+        formerror.textContent = "Please enter your college."
+        returnval = false;
+    }
+
+    if (branch === "") {
+        formerror.textContent = "Please select your branch."
+        returnval = false;
+    }
+
+    var phone = document.forms['myForm']["fphone"].value;
+    if (phone.length != 10) {
+        seterror("phone", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+
+    return returnval;
+}
+
+// confirmation message after submission of the form
+if (returnval = true) {
+    document.getElementById('submit').addEventListener('click', function (e) {
+        document.querySelector(".container").style.display = "none";
+        document.getElementById('confirmation-message').style.display = 'block';
+
+    })
+
+}
+
